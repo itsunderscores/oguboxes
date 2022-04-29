7fbd3a071ad8e3f225c6b06a549ed76e1c3a13e1
Underscores <84211119+itsunderscores@users.noreply.github.com>
2022-04-29 16:42:07 -0500

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,24 +1,27 @@
 // ==UserScript==
 // @name       remove boxes by underscores
-// @match      https://ogu.gg/
+// @match      https://ogu.gg/*
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
-    style = document.createElement('style');
+
+function GM_addStyle(css) {
+  const style = document.getElementById("GM_addStyleBy8626") || (function() {
+    const style = document.createElement('style');
     style.type = 'text/css';
-    style.innerHTML = css;
-    head.appendChild(style);
+    style.id = "GM_addStyleBy8626";
+    document.head.appendChild(style);
+    return style;
+  })();
+  const sheet = style.sheet;
+  sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
 }
 
 $(document).ready(function() {
     document.body.innerHTML = document.body.innerHTML.replace(/border: 1px solid #444;/g, '');
-    addGlobalStyle('.index_stats_item { border:none; !important }');
-    addGlobalStyle('.ilast { border:none; !important }');
-    addGlobalStyle('.wrapped { border:none; !important }');
-    addGlobalStyle('.statusblock { border:none; !important }');
-    addGlobalStyle('.user-service--member-profile { border:none; !important }');
+    GM_addStyle('.index_stats_item { border:none; !important }');
+    GM_addStyle('.ilast { border:none; !important }');
+    GM_addStyle('.wrapped { border:none; !important }');
+    GM_addStyle('.statusblock { border:none; !important }');
+    GM_addStyle('.user-service--member-profile { border:none; !important }');
 });
