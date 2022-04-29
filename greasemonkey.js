11ec669e9223b41901a917d946a511a1503a77c8
Underscores <84211119+itsunderscores@users.noreply.github.com>
2022-04-29 18:11:45 -0500

    Update greasemonkey.js

--- greasemonkey.js
+++ greasemonkey.js
@@ -2,6 +2,7 @@
 // @name Underscores Removed Boxes
 // @match https://ogu.gg/*
 // @include https://ogu.gg/*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js 
 // ==/UserScript==
 
 function addGlobalStyle(css) {
@@ -14,8 +15,19 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
+$("#cat_1 > table").css("border", "none");
+$("#cat_11 > table").css("border", "none");
+$("#cat_18 > table").css("border", "none");
+$("#cat_33 > table").css("border", "none");
+$("#cat_34 > table").css("border", "none");
+$("#cat_145 > table").css("border", "none");
+$("#cat_40 > table").css("border", "none");
+$(".sidebarstats").css("border", "none");
+$(".table-rounded").css("border", "none");
 
-document.body.innerHTML = document.body.innerHTML.replace(/border: 1px solid #444;/g, '');
+//If you have other boxes shown, use this
+//document.body.innerHTML = document.body.innerHTML.replace(/border: 1px solid #444;/g, '');
+
 addGlobalStyle('.index_stats_item { border:none; !important }');
 addGlobalStyle('.ilast { border:none; !important }');
 addGlobalStyle('.wrapped { border:none; !important }');
