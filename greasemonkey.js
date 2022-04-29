fba751fbdcb55c97d72c443e35d4164169a6b53f
Underscores <84211119+itsunderscores@users.noreply.github.com>
2022-04-29 16:33:20 -0500

    Create greasemonkey.js

---
+++ greasemonkey.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @grant          GM_addStyle
+// @match https://ogu.gg/*
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+
+console.log("hello");
+document.body.innerHTML = document.body.innerHTML.replace(/border: 1px solid #444;/g, '');
+addGlobalStyle('.index_stats_item { border:none; !important }');
+addGlobalStyle('.ilast { border:none; !important }');
+addGlobalStyle('.wrapped { border:none; !important }');
+addGlobalStyle('.statusblock { border:none; !important }');
+addGlobalStyle('.user-service--member-profile { border:none; !important }');
