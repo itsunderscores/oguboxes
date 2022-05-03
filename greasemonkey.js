478eaed298eaa30b065159b64e772c38b940ea31
Underscores <84211119+itsunderscores@users.noreply.github.com>
2022-05-02 22:03:43 -0500

    Update greasemonkey.js

--- greasemonkey.js
+++ greasemonkey.js
@@ -15,7 +15,10 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
-//Main Page
+//document.body.innerHTML = document.body.innerHTML.replace(/border: 1px solid #444;/g, ''); //If you have other boxes shown, unquote this
+document.body.innerHTML = document.body.innerHTML.replace(/border-radius: 4px;/g, 'border-radius: 50%;'); //Fixes Profile Pictures
+
+//Home Page
 $("#cat_1 > table").css("border", "none");
 $("#cat_11 > table").css("border", "none");
 $("#cat_18 > table").css("border", "none");
@@ -33,12 +36,18 @@ $(".wrapped").css("border", "none");
 $(".statusblock").css("border", "none");
 $(".user-service--member-profile").css("border", "none");
 
-//Misc Fixes
+//Threads/Posts
 $(".post_body").css("font-size", "14px"); //Text was made bigger, reduced down to normal size
-$(".smalltext").css("font-size", "10px"); //Replaces Small Text
+$(".smalltext").css("font-size", "12px"); //Replaces Small Text
 $(".mycode_size").css("font-size", "14px"); //Replaced overriden medium/large text
 $(".footer-nav h3").css("font-size", "14px"); //Replaced footer text size
 $(".post_body").css("padding", "10px 0"); //Fixed spacing on threads
 
 
-//document.body.innerHTML = document.body.innerHTML.replace(/border: 1px solid #444;/g, ''); //If you have other boxes shown, unquote this
+/*
+addGlobalStyle('.index_stats_item { border:none; !important }');
+addGlobalStyle('.ilast { border:none; !important }');
+addGlobalStyle('.wrapped { border:none; !important }');
+addGlobalStyle('.statusblock { border:none; !important }');
+addGlobalStyle('.user-service--member-profile { border:none; !important }');
+*/
