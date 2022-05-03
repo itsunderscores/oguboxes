72a4d876716ce38beef888dc8f2dacdd2014240b
Underscores <84211119+itsunderscores@users.noreply.github.com>
2022-05-02 22:07:50 -0500

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -18,10 +18,29 @@ function GM_addStyle(css) {
 }
 
 $(document).ready(function() {
-    document.body.innerHTML = document.body.innerHTML.replace(/border: 1px solid #444;/g, '');
-    GM_addStyle('.index_stats_item { border:none; !important }');
-    GM_addStyle('.ilast { border:none; !important }');
-    GM_addStyle('.wrapped { border:none; !important }');
-    GM_addStyle('.statusblock { border:none; !important }');
-    GM_addStyle('.user-service--member-profile { border:none; !important }');
+    document.body.innerHTML = document.body.innerHTML.replace(/border-radius: 4px;/g, 'border-radius: 50%;'); //Fixes Profile Pictures
+
+    $("#cat_1 > table").css("border", "none");
+    $("#cat_11 > table").css("border", "none");
+    $("#cat_18 > table").css("border", "none");
+    $("#cat_33 > table").css("border", "none");
+    $("#cat_34 > table").css("border", "none");
+    $("#cat_145 > table").css("border", "none");
+    $("#cat_40 > table").css("border", "none");
+    $(".sidebarstats").css("border", "none");
+    $(".table-rounded").css("border", "none");
+
+    //Profile
+    $(".index_stats_item").css("border", "none");
+    $(".ilast").css("border", "none");
+    $(".wrapped").css("border", "none");
+    $(".statusblock").css("border", "none");
+    $(".user-service--member-profile").css("border", "none");
+
+    //Threads/Posts
+    $(".post_body").css("font-size", "14px"); //Text was made bigger, reduced down to normal size
+    $(".smalltext").css("font-size", "12px"); //Replaces Small Text
+    $(".mycode_size").css("font-size", "14px"); //Replaced overriden medium/large text
+    $(".footer-nav h3").css("font-size", "14px"); //Replaced footer text size
+    $(".post_body").css("padding", "10px 0"); //Fixed spacing on threads
 });
